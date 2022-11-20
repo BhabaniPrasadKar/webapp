526cf5d3bb308f3276e2ee0a65f40b32cd72f2fd
BhabaniPrasadKar <52153583+BhabaniPrasadKar@users.noreply.github.com>
2022-11-20 17:11:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home -Azure Pipelines Continuous delivery</h3>"
+    html = "<h3>Sklearn Prediction: -Azure Pipelines Continuous delivery</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
